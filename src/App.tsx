cc6288eb4c9e58b4b1a2485864624588c1bbf82b
Carlos S. <carlossalmeroncabrera@gmail.com>
2024-07-17 13:02:24 -0600

    Added Router to App.tsx and created styles folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
+import Router from "./Router";
+
 function App() {
   return (
     <>
-      <div>hola</div>
-      <h1 className="rounded-xl shadow-lg hover:bg-blue-500 p-2">
-        Hello world!
-      </h1>
+      <Router />
     </>
   );
 }
