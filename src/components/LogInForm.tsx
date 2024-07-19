3ba7eca40c4291b1303f21a57ad251add6abfe42
Carlos S. <carlossalmeroncabrera@gmail.com>
2024-07-19 17:16:03 -0600

    Fixed LogInForm

--- src/components/LogInForm.tsx
+++ src/components/LogInForm.tsx
@@ -1,20 +1,14 @@
 import React, { useState, ChangeEvent, FormEvent } from "react";
 
 interface FormData {
-  name: string;
   email: string;
   password: string;
-  phone: string;
-  age: string;
 }
 
-const LoginForm: React.FC = () => {
+const LogInForm: React.FC = () => {
   const [formData, setFormData] = useState<FormData>({
-    name: "",
     email: "",
     password: "",
-    phone: "",
-    age: "",
   });
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
@@ -36,19 +30,8 @@ const LoginForm: React.FC = () => {
       onSubmit={handleSubmit}
       className="w-full bg-purple-600 rounded-md flex flex-col text-white p-4"
     >
-      <div className="mb-4 text-center text-lg font-bold">Log In Form</div>
-
-      <div className="grid grid-cols-[1fr,3fr] gap-4 mb-4 items-center">
-        <label className="mb-2">Name</label>
-        <input
-          type="text"
-          name="name"
-          value={formData.name}
-          onChange={handleChange}
-          className="p-2 border-2 border-transparent rounded-md text-purple-700 bg-white placeholder-gray-500 focus:outline-none focus:border-purple-900 focus:ring-1 focus:ring-purple-700"
-          placeholder="Name"
-          required
-        />
+      <div className="mb-4 text-center text-lg font-bold">
+        Ingresa con tu cuenta
       </div>
 
       <div className="grid grid-cols-[1fr,3fr] gap-4 mb-4 items-center">
@@ -77,40 +60,14 @@ const LoginForm: React.FC = () => {
         />
       </div>
 
-      <div className="grid grid-cols-[1fr,3fr] gap-4 mb-4 items-center">
-        <label className="mb-2">Phone</label>
-        <input
-          type="tel"
-          name="phone"
-          value={formData.phone}
-          onChange={handleChange}
-          className="p-2 border-2 border-transparent rounded-md text-purple-700 bg-white placeholder-gray-500 focus:outline-none focus:border-purple-900 focus:ring-1 focus:ring-purple-700"
-          placeholder="Phone"
-          required
-        />
-      </div>
-
-      <div className="grid grid-cols-[1fr,3fr] gap-4 mb-4 items-center">
-        <label className="mb-2">Age</label>
-        <input
-          type="number"
-          name="age"
-          value={formData.age}
-          onChange={handleChange}
-          className="p-2 border-2 border-transparent rounded-md text-purple-700 bg-white placeholder-gray-500 focus:outline-none focus:border-purple-900 focus:ring-1 focus:ring-purple-700"
-          placeholder="Age"
-          required
-        />
-      </div>
-
       <button
         type="submit"
         className="bg-white text-purple-600 p-2 rounded-md font-bold self-center px-8"
       >
-        Log In
+        Ingresar
       </button>
     </form>
   );
 };
 
-export default LoginForm;
+export default LogInForm;
