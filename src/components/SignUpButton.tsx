a02b34709266f29c5075c055a53a16e9e14906d7
Carlos S. <carlossalmeroncabrera@gmail.com>
2024-07-26 14:23:40 -0600

    Created a sign up button

---
+++ src/components/SignUpButton.tsx
@@ -0,0 +1,10 @@
+export default function SignUpButton() {
+  return (
+    <div
+      className="cursor-pointer rounded-md border border-green-300 bg-teal-600 bg-opacity-90 px-4 py-2 font-bold text-white transition-all hover:bg-teal-700"
+      onClick={() => {}}
+    >
+      Registrate
+    </div>
+  );
+}
