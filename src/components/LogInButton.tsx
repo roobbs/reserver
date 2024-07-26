cd114b0a8f12cef5895bd099d5070e33b5a772ec
Carlos S. <carlossalmeroncabrera@gmail.com>
2024-07-26 14:23:59 -0600

    Created a log in button

---
+++ src/components/LogInButton.tsx
@@ -0,0 +1,10 @@
+export default function LogInButton() {
+  return (
+    <div
+      className="cursor-pointer rounded-md border border-blue-900 bg-white px-4 py-2 font-bold text-blue-900 transition-all hover:bg-blue-950 hover:text-white"
+      onClick={() => {}}
+    >
+      Ingresa Ahora
+    </div>
+  );
+}
