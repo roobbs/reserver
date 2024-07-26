7a272ae77266f129b08dab155a9aa69c84b33f25
Carlos S. <carlossalmeroncabrera@gmail.com>
2024-07-26 15:31:52 -0600

    SignUpButton redirects to "Registro"

--- src/components/SignUpButton.tsx
+++ src/components/SignUpButton.tsx
@@ -1,8 +1,12 @@
+import { useNavigate } from "react-router-dom";
+
 export default function SignUpButton() {
+  const navigate = useNavigate();
+
   return (
     <div
       className="cursor-pointer rounded-md border border-green-300 bg-teal-600 bg-opacity-90 px-4 py-2 font-bold text-white transition-all hover:bg-teal-700"
-      onClick={() => {}}
+      onClick={() => navigate("/registro")}
     >
       Registrate
     </div>
