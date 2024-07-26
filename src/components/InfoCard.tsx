1cf7c3f198702839725c232f6273cb661d3ea1e6
Carlos S. <carlossalmeroncabrera@gmail.com>
2024-07-26 15:12:41 -0600

    Created InfoCard.tsx

---
+++ src/components/InfoCard.tsx
@@ -0,0 +1,22 @@
+import { CiCircleCheck } from "react-icons/ci";
+
+interface InfoCardProps {
+  title: string;
+  info: string[];
+}
+
+export default function InfoCard({ title, info }: InfoCardProps) {
+  return (
+    <div className="w-fit max-w-64 rounded-md bg-white p-4 text-black shadow-sm shadow-white">
+      <div className="mb-4 flex items-center gap-2 border-b border-b-sky-900 pb-4">
+        <CiCircleCheck size={30} />
+        <div className="font-semibold">{title}</div>
+      </div>
+      <div className="flex flex-col gap-4">
+        {info.map((el) => (
+          <div className="text-gray-600">{el}</div>
+        ))}
+      </div>
+    </div>
+  );
+}
