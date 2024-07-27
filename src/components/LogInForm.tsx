fbc27eab21134d04086b217bbb73c5ef88c3efb1
Carlos S. <carlossalmeroncabrera@gmail.com>
2024-07-27 14:43:43 -0600

    Modified log in form

--- src/components/LogInForm.tsx
+++ src/components/LogInForm.tsx
@@ -1,11 +1,12 @@
-import React, { useState, ChangeEvent, FormEvent } from "react";
+import { useState, ChangeEvent, FormEvent } from "react";
+import "../styles/Form.css";
 
 interface FormData {
   email: string;
   password: string;
 }
 
-const LogInForm: React.FC = () => {
+function LogInForm() {
   const [formData, setFormData] = useState<FormData>({
     email: "",
     password: "",
@@ -28,33 +29,27 @@ const LogInForm: React.FC = () => {
   return (
     <form
       onSubmit={handleSubmit}
-      className="w-full bg-purple-600 rounded-md flex flex-col text-white p-4"
+      className="formComponent flex w-fit flex-col gap-4 rounded-md border border-white p-4"
     >
-      <div className="mb-4 text-center text-lg font-bold">
-        Ingresa con tu cuenta
-      </div>
-
-      <div className="grid grid-cols-[1fr,3fr] gap-4 mb-4 items-center">
-        <label className="mb-2">Email</label>
+      <div className="inputContainer">
+        <label>Correo</label>
         <input
           type="email"
           name="email"
           value={formData.email}
           onChange={handleChange}
-          className="p-2 border-2 border-transparent rounded-md text-purple-700 bg-white placeholder-gray-500 focus:outline-none focus:border-purple-900 focus:ring-1 focus:ring-purple-700"
           placeholder="Email"
           required
         />
       </div>
 
-      <div className="grid grid-cols-[1fr,3fr] gap-4 mb-4 items-center">
-        <label className="mb-2">Password</label>
+      <div className="inputContainer">
+        <label>Contraseña</label>
         <input
           type="password"
           name="password"
           value={formData.password}
           onChange={handleChange}
-          className="p-2 border-2 border-transparent rounded-md text-purple-700 bg-white placeholder-gray-500 focus:outline-none focus:border-purple-900 focus:ring-1 focus:ring-purple-700"
           placeholder="Password"
           required
         />
@@ -62,12 +57,12 @@ const LogInForm: React.FC = () => {
 
       <button
         type="submit"
-        className="bg-white text-purple-600 p-2 rounded-md font-bold self-center px-8"
+        className="m-3 rounded-md bg-white px-2 py-1 text-blue-900"
       >
         Ingresar
       </button>
     </form>
   );
-};
+}
 
 export default LogInForm;
