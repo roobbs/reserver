921e60a3318c26503076121d3b259801093cb363
Carlos S. <carlossalmeroncabrera@gmail.com>
2024-07-27 11:06:19 -0600

    Modified padding on sign up screen

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -3,7 +3,7 @@ import { CiCalendar } from "react-icons/ci";
 
 export default function SignUp() {
   return (
-    <div className="background relative flex flex-1 flex-col place-content-center place-items-center gap-4">
+    <div className="background relative flex flex-1 flex-col place-content-center place-items-center gap-4 p-6">
       <div className="text-3xl font-semibold text-cyan-400">Crea tu cuenta</div>
       <SignUpForm />
       <CiCalendar
