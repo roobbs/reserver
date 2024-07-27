4194c509cc4349ef2e32d989a8b18bd24fa154cc
Carlos S. <carlossalmeroncabrera@gmail.com>
2024-07-27 14:44:37 -0600

    LogInButton redirects to LogIn screen

--- src/components/LogInButton.tsx
+++ src/components/LogInButton.tsx
@@ -1,8 +1,12 @@
+import { useNavigate } from "react-router-dom";
+
 export default function LogInButton() {
+  const navigate = useNavigate();
+
   return (
     <div
       className="cursor-pointer rounded-md border border-blue-900 bg-white px-4 py-2 font-bold text-blue-900 transition-all hover:bg-blue-950 hover:text-white"
-      onClick={() => {}}
+      onClick={() => navigate("/ingresar")}
     >
       Ingresa Ahora
     </div>
