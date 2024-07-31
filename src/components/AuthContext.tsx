dc897dbf312f1ff839c3e2efffb12aa928c90f3b
Carlos S. <carlossalmeroncabrera@gmail.com>
2024-07-30 21:51:21 -0600

    Created Auth Context for saving user data

---
+++ src/components/AuthContext.tsx
@@ -0,0 +1,47 @@
+import { createContext, useState, ReactNode } from "react";
+
+interface User {
+  _id: string;
+  first_name: string;
+  last_name: string;
+  email: string;
+  googleId: string;
+  createdAt: Date;
+  updatedAt: Date;
+  __v: number;
+}
+
+interface AuthContextType {
+  user: User | null;
+  addUser: (user: User) => void;
+  logOutUser: () => void;
+}
+
+interface AuthProviderProps {
+  children: ReactNode;
+}
+
+export const AuthContext = createContext<AuthContextType>({
+  user: null,
+  addUser: () => {},
+  logOutUser: () => {},
+});
+
+export default function AuthProvider({ children }: AuthProviderProps) {
+  const [user, setUser] = useState<User | null>(null);
+
+  const addUser = (user: User) => {
+    setUser(user);
+  };
+
+  const logOutUser = () => {
+    setUser(null);
+    localStorage.removeItem("jwtToken");
+  };
+
+  return (
+    <AuthContext.Provider value={{ user, addUser, logOutUser }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
