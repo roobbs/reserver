1a48c8ab9569f118adce023d273712ac8f20a953
Carlos S. <carlossalmeroncabrera@gmail.com>
2024-07-31 10:53:29 -0600

    Modified SignUp screen

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -1,14 +1,23 @@
 import SignUpForm from "../components/SignUpForm";
 import { CiCalendar } from "react-icons/ci";
+import GoogleAuthButton from "../components/GoogleAuthButton";
+import { useNavigate } from "react-router-dom";
 
 export default function SignUp() {
+  const navigate = useNavigate();
+
   return (
     <div className="background relative flex flex-1 flex-col place-content-center place-items-center gap-4 p-6">
       <div className="text-3xl font-semibold text-cyan-400">Crea tu cuenta</div>
+      <GoogleAuthButton />
+      <div className="text-gray-400">O registrate aqui:</div>
       <SignUpForm />
       <CiCalendar
+        onClick={() => {
+          navigate("/");
+        }}
         size={40}
-        className="absolute"
+        className="absolute cursor-pointer hover:text-emerald-400"
         style={{ top: "20px", left: "30px" }}
       />
     </div>
