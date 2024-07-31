e8999fbdbbf33eda9842c4d6a7c6f36993aa998f
Carlos S. <carlossalmeroncabrera@gmail.com>
2024-07-30 21:54:05 -0600

    Created PrivateRoute to check if user is logged in all routes

---
+++ src/components/PrivateRoute.tsx
@@ -0,0 +1,11 @@
+import { useContext } from "react";
+import { Navigate, Outlet } from "react-router-dom";
+import { AuthContext } from "./AuthContext";
+
+const PrivateRoute = () => {
+  const { user } = useContext(AuthContext);
+
+  return user ? <Outlet /> : <Navigate to="/" />;
+};
+
+export default PrivateRoute;
