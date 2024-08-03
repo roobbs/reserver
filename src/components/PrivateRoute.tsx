a54c49d81e5ef00f9807948615311da68286edd6
Carlos S. <carlossalmeroncabrera@gmail.com>
2024-08-02 22:08:30 -0600

    Added HomeHeader to PrivateRoute.tsx

--- src/components/PrivateRoute.tsx
+++ src/components/PrivateRoute.tsx
@@ -2,11 +2,19 @@ import { useContext } from "react";
 import { Outlet } from "react-router-dom";
 import { AuthContext } from "./AuthContext";
 import AuthError from "../screens/AuthError";
+import HomeHeader from "./HomeHeader";
 
 const PrivateRoute = () => {
   const { user } = useContext(AuthContext);
 
-  return user ? <Outlet /> : <AuthError />;
+  return user ? (
+    <>
+      <HomeHeader name={user.first_name} />
+      <Outlet />
+    </>
+  ) : (
+    <AuthError />
+  );
 };
 
 export default PrivateRoute;
