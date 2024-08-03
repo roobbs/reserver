4bebc6a44e80051773d0fc64dd75ff32d0c5764b
Carlos S. <carlossalmeroncabrera@gmail.com>
2024-08-02 22:42:31 -0600

    Created buttons section

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,7 +1,52 @@
+import { useContext, useState } from "react";
+import { AuthContext } from "../components/AuthContext";
+import { ImProfile } from "react-icons/im";
+import { MdOutlineAddBusiness } from "react-icons/md";
+import { TbCalendarUser } from "react-icons/tb";
+import "../styles/Home.css";
+
 export default function Home() {
+  const [show, setShow] = useState("profile");
+  const { user } = useContext(AuthContext);
+
   return (
-    <div>
-      <div>hola esto es home</div>
-    </div>
+    <>
+      <main className="flex flex-1 flex-col">
+        <section className="flex justify-around gap-12 bg-white px-8 py-2 text-blue-900">
+          <div
+            onClick={() => setShow("profile")}
+            className={
+              show === "profile"
+                ? "sectionButton bg-blue-950 text-white transition-all"
+                : "sectionButton transition-all"
+            }
+          >
+            <ImProfile size={30} />
+            Perfil
+          </div>
+          <div
+            onClick={() => setShow("business")}
+            className={
+              show === "business"
+                ? "sectionButton bg-blue-950 text-white transition-all"
+                : "sectionButton transition-all"
+            }
+          >
+            <MdOutlineAddBusiness size={30} /> Negocios
+          </div>
+          <div
+            onClick={() => setShow("dates")}
+            className={
+              show === "dates"
+                ? "sectionButton bg-blue-950 text-white transition-all"
+                : "sectionButton transition-all"
+            }
+          >
+            <TbCalendarUser size={30} /> Tus citas
+          </div>
+        </section>
+        {user && <div>bienvenido {user.first_name}</div>}
+      </main>
+    </>
   );
 }
