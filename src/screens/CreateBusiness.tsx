af479333106aaadb23d198a96090c5d0d74192e0
Carlos S. <carlossalmeroncabrera@gmail.com>
2024-08-06 16:29:13 -0600

    Created screen for Adding a service provider

---
+++ src/screens/CreateBusiness.tsx
@@ -0,0 +1,12 @@
+import ProviderForm from "../components/ProviderForm";
+
+export default function CreateBusiness() {
+  return (
+    <div className="background relative flex flex-1 flex-col place-content-center place-items-center gap-4 p-6">
+      <div className="text-3xl font-semibold text-cyan-400">
+        Ingresa la información de tu negocio
+      </div>
+      <ProviderForm />
+    </div>
+  );
+}
