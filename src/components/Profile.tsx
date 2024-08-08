a4485f9622c90fac62d302aaed6164b25c7d862d
Carlos S. <carlossalmeroncabrera@gmail.com>
2024-08-08 17:47:08 -0600

    Modified Profile.tsx styles

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -13,18 +13,21 @@ export default function Profile({ fn }: ProfileProps) {
   const { user, business } = useContext(AuthContext);
 
   return (
-    <div className="flex flex-1 flex-col justify-between gap-12 p-4 py-8">
-      <div className="flex justify-around">
-        <div className="rounded-sm bg-white p-12 text-slate-600">
-          Tienes 0 mensajes
+    <div className="flex flex-1 flex-col justify-between gap-10 p-4 py-8">
+      <section className="flex flex-col gap-4 rounded bg-white p-8">
+        <div className="text-xl font-bold text-blue-900">Tú información:</div>
+        <div className="flex justify-around rounded">
+          <div className="rounded-sm bg-slate-100 p-12 text-slate-600 shadow-sm shadow-slate-600">
+            Tienes 0 mensajes
+          </div>
+          <div className="rounded-sm bg-slate-100 p-12 text-slate-600 shadow-sm shadow-slate-600">
+            Tienes 0 citas pendientes
+          </div>
+          <div className="rounded-sm bg-slate-100 p-12 text-slate-600 shadow-sm shadow-slate-600">
+            Ver historial de citas
+          </div>
         </div>
-        <div className="rounded-sm bg-white p-12 text-slate-600">
-          Tienes 0 citas pendientes
-        </div>
-        <div className="rounded-sm bg-white p-12 text-slate-600">
-          Ver historial de citas
-        </div>
-      </div>
+      </section>
 
       <div
         onClick={fn}
