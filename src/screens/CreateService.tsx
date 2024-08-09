41fb4585e71a19f62edb37d76a196acf31c5db42
Carlos S. <carlossalmeroncabrera@gmail.com>
2024-08-09 16:19:41 -0600

    Created CreateService.tsx screen

---
+++ src/screens/CreateService.tsx
@@ -0,0 +1,12 @@
+import ServiceForm from "../components/ServiceForm";
+
+export default function CreateService() {
+  return (
+    <div className="background relative flex flex-1 flex-col place-content-center place-items-center gap-4 p-6">
+      <div className="text-3xl font-semibold text-cyan-400">
+        Agrega un nuevo servicio
+      </div>
+      <ServiceForm />
+    </div>
+  );
+}
