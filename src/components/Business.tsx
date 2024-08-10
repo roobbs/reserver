89017118fde7c13ce0d2fba218deb2e4483d7558
Carlos S. <carlossalmeroncabrera@gmail.com>
2024-08-10 15:38:43 -0600

    Modified Business.tsx to render all businesses

--- src/components/Business.tsx
+++ src/components/Business.tsx
@@ -1,7 +1,17 @@
+import { useContext } from "react";
+import { AuthContext } from "../components/AuthContext";
+import BusinessCard from "./BusinessCard";
+
 export default function Business() {
+  const { businessesList } = useContext(AuthContext);
+
   return (
-    <>
-      <div>Business</div>
-    </>
+    <div className="flex flex-1 flex-col gap-4 p-6">
+      <div className="text-xl">Descubre nuevos servicios:</div>
+      <div className="flex flex-col gap-4">
+        {businessesList &&
+          businessesList.map((el) => <BusinessCard business={el} />)}
+      </div>
+    </div>
   );
 }
