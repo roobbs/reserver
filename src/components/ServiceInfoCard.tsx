935f394f5a82448c0279f07daabf708cad7d1039
Carlos S. <carlossalmeroncabrera@gmail.com>
2024-08-16 22:17:13 -0600

    Modified ServiceInfoCard to reserve service

--- src/components/ServiceInfoCard.tsx
+++ src/components/ServiceInfoCard.tsx
@@ -1,16 +1,31 @@
 import { Service } from "./AuthContext";
+import { useNavigate } from "react-router-dom";
 
 interface ServiceInfoProps {
   service: Service;
 }
 
 export default function ServiceInfoCard({ service }: ServiceInfoProps) {
+  const navigate = useNavigate();
+
+  function goToService() {
+    if (service._id) {
+      navigate(`/home/service/${service._id}`, { state: { service } });
+    }
+  }
+
   return (
     <div className="flex cursor-pointer flex-col gap-2 rounded border border-slate-400 p-4 shadow-md shadow-slate-600 transition-all hover:bg-blue-950 hover:text-white">
       <div className="self-center text-lg font-bold">{service.name}</div>
       <div className="italic">{service.description} </div>
-      <div>Listo en: {service.duration}min.</div>
+      <div>Tarda: {service.duration}min.</div>
       <div className="self-center text-2xl font-bold">${service.price} </div>
+      <div
+        onClick={() => goToService()}
+        className="m-1 self-center rounded border border-white bg-emerald-800 p-1 px-2 text-white shadow-sm transition-all hover:border-green-900 hover:bg-emerald-500"
+      >
+        RESERVAR
+      </div>
     </div>
   );
 }
