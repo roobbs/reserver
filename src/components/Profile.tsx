6053aad685424fcb67aebe0c909506114ce4ec15
Carlos S. <carlossalmeroncabrera@gmail.com>
2024-08-23 15:54:12 -0600

    Change style of Profile.tsx and added new function

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -6,10 +6,11 @@ import { Link } from "react-router-dom";
 import BusinessSection from "./BusinessSection";
 
 interface ProfileProps {
-  fn: () => void;
+  serviceFn: () => void;
+  appointmentFn: () => void;
 }
 
-export default function Profile({ fn }: ProfileProps) {
+export default function Profile({ serviceFn, appointmentFn }: ProfileProps) {
   const { user, business, appointments } = useContext(AuthContext);
 
   return (
@@ -17,24 +18,27 @@ export default function Profile({ fn }: ProfileProps) {
       <section className="flex flex-col gap-4 rounded bg-white p-8">
         <div className="text-xl font-bold text-blue-900">Tú información:</div>
         <div className="flex justify-around rounded">
-          <div className="rounded-sm bg-slate-100 p-12 text-slate-600 shadow-sm shadow-slate-600">
+          <div className="rounded-sm bg-slate-100 p-12 text-slate-600 shadow-sm shadow-slate-600 transition-all hover:bg-slate-900 hover:text-white">
             Tienes 0 mensajes
           </div>
-          <div className="rounded-sm bg-slate-100 p-12 text-slate-600 shadow-sm shadow-slate-600">
+          <div
+            onClick={appointmentFn}
+            className="cursor-pointer rounded-sm bg-slate-100 p-12 text-slate-600 shadow-sm shadow-slate-600 transition-all hover:bg-slate-900 hover:text-white"
+          >
             Tienes {appointments?.length} citas pendiente(s)
           </div>
-          <div className="rounded-sm bg-slate-100 p-12 text-slate-600 shadow-sm shadow-slate-600">
+          <div className="rounded-sm bg-slate-100 p-12 text-slate-600 shadow-sm shadow-slate-600 transition-all hover:bg-slate-900 hover:text-white">
             Ver historial de citas
           </div>
         </div>
       </section>
 
       <div
-        onClick={fn}
+        onClick={serviceFn}
         className="flex cursor-pointer items-center gap-2 self-center rounded-md border border-transparent bg-emerald-600 px-4 py-1 text-lg transition-all hover:border-green-500 hover:bg-emerald-800"
       >
         <MdOutlineManageSearch size={30} />
-        Busca un servicio aqui!
+        Explora los servicios disponibles aqui!
       </div>
 
       {!user?.service_provider && (
