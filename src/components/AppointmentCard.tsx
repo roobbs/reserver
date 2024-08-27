fd997700b0450fc3b3010ee264f46c603ea1f949
Carlos S. <carlossalmeroncabrera@gmail.com>
2024-08-27 12:26:57 -0600

    Modified AppointmentCard style

--- src/components/AppointmentCard.tsx
+++ src/components/AppointmentCard.tsx
@@ -38,18 +38,45 @@ export default function AppointmentCard({
     }
   };
 
+  const options: Intl.DateTimeFormatOptions = {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  };
+
   return (
     <div className="rounded bg-white p-4 text-blue-950">
-      <div>Tu cita es el dia: {appointment.date}</div>
-      <div>a la siguiente hora: {appointment.time}</div>
-      <div>Servicio adquirido: {appointment.providerId.name}</div>
-      <div>$ {appointment.serviceId.price}</div>
-      <div>{appointment.serviceId.name}</div>
+      <div className="mb-1 border-b border-slate-400 pb-1">
+        Tu cita es el dia:{" "}
+        <strong className="text-lg">
+          {new Date(appointment.date).toLocaleDateString("es-ES", options)}
+        </strong>
+      </div>
+      <div className="flex flex-col gap-0">
+        <div className="text-sm">HORA:</div>
+        <div className="self-center rounded border bg-slate-900 p-1 text-xl font-bold text-white">
+          {appointment.time}
+        </div>
+      </div>
+      <div className="flex flex-col gap-0 border-b border-slate-400 pb-2">
+        <div className="text-sm">SERVICIO ADQUIRIDO:</div>
+        <div className="flex justify-around">
+          <div className="text-xl font-bold">{appointment.providerId.name}</div>
+          <div className="text-xl font-bold">
+            $ {appointment.serviceId.price}
+          </div>
+        </div>
+      </div>
+
+      <div className="my-2 text-center font-bold text-slate-700">
+        {appointment.serviceId.name}
+      </div>
       <div className="flex items-center justify-between">
         <div className="w-fit cursor-pointer rounded bg-emerald-600 p-2 text-white transition-all hover:bg-emerald-700">
           Enviar mensaje
         </div>
-        <div
+        <button
           className={`w-fit cursor-pointer rounded bg-red-800 p-2 text-sm text-white transition-all hover:bg-red-600 ${
             isCanceling ? "cursor-not-allowed opacity-50" : ""
           }`}
@@ -57,7 +84,7 @@ export default function AppointmentCard({
           disabled={isCanceling}
         >
           {isCanceling ? "Cancelando..." : "Cancelar cita"}
-        </div>
+        </button>
       </div>
     </div>
   );
