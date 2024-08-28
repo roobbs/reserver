fb71188f839073c0535358804079cd275ed72eda
Carlos S. <carlossalmeroncabrera@gmail.com>
2024-08-28 10:16:03 -0600

    Added PastAppointmetCard.tsx toAppointments screen

--- src/components/Appointments.tsx
+++ src/components/Appointments.tsx
@@ -1,17 +1,20 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { AuthContext } from "./auth/AuthContext";
 import AppointmentCard from "./AppointmentCard";
+import PastAppointmentCard from "./PastAppoinmentCard";
 import { IoToday } from "react-icons/io5";
 import { IoIosToday } from "react-icons/io";
+import { CiCircleChevUp, CiCircleChevDown } from "react-icons/ci";
 
 export default function Appointments() {
   const { appointments } = useContext(AuthContext);
+  const [showPastAppointments, setShowPastAppointments] = useState(false);
 
   const today = new Date().toISOString().split("T")[0];
 
   const pastAppointments = appointments
     ? appointments.filter(
-        (a) => new Date(a.date) < new Date(today) && a.status !== "canceled",
+        (a) => new Date(a.date) < new Date(today) || a.status === "canceled",
       )
     : [];
   const todayAppointments = appointments
@@ -55,15 +58,21 @@ export default function Appointments() {
         </div>
       )}
       {pastAppointments.length > 0 && (
-        <div className="flex flex-col gap-2">
-          <h3 className="flex cursor-pointer items-center gap-2 border-b pl-4 text-3xl">
-            Citas Pasadas
+        <div className="flex flex-col gap-4 rounded bg-white p-4 text-slate-900">
+          <h3
+            onClick={() => setShowPastAppointments(!showPastAppointments)}
+            className="flex cursor-pointer items-center gap-2 border-b border-b-slate-400 pb-2 text-3xl hover:text-blue-900"
+          >
+            Historial de citas{" "}
+            {showPastAppointments ? <CiCircleChevUp /> : <CiCircleChevDown />}
           </h3>
-          <div className="flex justify-center gap-6">
-            {pastAppointments.map((a, index) => (
-              <AppointmentCard key={index} appointment={a} />
-            ))}
-          </div>
+          {showPastAppointments && (
+            <div className="flex justify-center gap-6">
+              {pastAppointments.map((a, index) => (
+                <PastAppointmentCard key={index} appointment={a} />
+              ))}
+            </div>
+          )}
         </div>
       )}
       {appointments?.length === 0 && (
