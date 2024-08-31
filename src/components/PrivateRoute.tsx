2732f90e2520b25499ca9576871b42bbe6e3175a
Carlos S. <carlossalmeroncabrera@gmail.com>
2024-08-31 17:33:48 -0600

    Wrapped PrivateRoute.tsx into SocketProvider

--- src/components/PrivateRoute.tsx
+++ src/components/PrivateRoute.tsx
@@ -4,16 +4,17 @@ import { AuthContext } from "./auth/AuthContext";
 import AuthError from "../screens/error/AuthError";
 import HomeHeader from "./HomeHeader";
 import Menu from "./Menu";
+import SocketProvider from "./socket/SocketContext";
 
 const PrivateRoute = () => {
   const { user } = useContext(AuthContext);
 
   return user ? (
-    <>
+    <SocketProvider>
       <HomeHeader name={user.first_name} />
       <Menu />
       <Outlet />
-    </>
+    </SocketProvider>
   ) : (
     <AuthError />
   );
