540926c0c8489aabfdc8c970d06d8352a14fe490
Carlos S. <carlossalmeroncabrera@gmail.com>
2024-09-02 17:47:01 -0600

    Change Business.tsx style

--- src/screens/Business.tsx
+++ src/screens/Business.tsx
@@ -6,7 +6,7 @@ export default function Business() {
   const { businessesList } = useContext(AuthContext);
 
   return (
-    <div className="font flex flex-1 flex-col gap-8 bg-gray-200 p-6 text-blue-950">
+    <div className="flex flex-1 flex-col gap-8 bg-gray-200 p-6 text-blue-950">
       <div className="text-xl font-bold">Descubre nuevos servicios:</div>
       <div className="flex flex-col gap-4">
         {businessesList &&
