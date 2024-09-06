4797c5af11080805f8bcc34292861870efaba9f7
Carlos S. <carlossalmeroncabrera@gmail.com>
2024-09-06 15:02:58 -0600

    Added hour and mofied style in ReceiveMessageCard.tsx

--- src/components/message/ReceivedMessageCard.tsx
+++ src/components/message/ReceivedMessageCard.tsx
@@ -1,17 +1,30 @@
 interface Message {
   senderId: string;
-  // receiverId: string;
   conversationId: string;
   content: string;
   createdAt: string;
 }
 
 export default function ReceivedMessageCard({ message }: { message: Message }) {
+  const messageDate = new Date(message.createdAt);
+
+  const today = new Date();
+  today.setHours(0, 0, 0, 0);
+
+  const isToday = messageDate >= today;
+
   return (
-    <div className="max-w-[70%] self-start rounded-xl border border-blue-950 bg-white px-4 py-2 text-blue-800">
-      <div className="font-bold">{message.content}</div>
+    <div className="flex max-w-[70%] flex-col gap-1 self-start">
+      <div className="rounded-xl border border-blue-950 bg-white px-4 py-2 text-blue-800">
+        <div className="font-bold">{message.content}</div>
+      </div>
       <div className="text-left text-sm">
-        {new Date(message.createdAt).toLocaleDateString()}
+        {isToday
+          ? messageDate.toLocaleTimeString([], {
+              hour: "2-digit",
+              minute: "2-digit",
+            })
+          : `${messageDate.toLocaleDateString()} ${messageDate.toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" })}`}
       </div>
     </div>
   );
