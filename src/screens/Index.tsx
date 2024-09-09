795acf6aea0b78ec3a1a6357f01af5deb8e94fe8
Carlos S. <carlossalmeroncabrera@gmail.com>
2024-09-09 13:23:28 -0600

    Modified index.tsx style

--- src/screens/Index.tsx
+++ src/screens/Index.tsx
@@ -29,10 +29,10 @@ const info = [
 
 function Index() {
   return (
-    <div className="background flex flex-1 flex-col gap-12 py-6 pl-8 text-white">
+    <div className="flex flex-1 flex-col gap-12 bg-white py-6 pl-8 text-blue-950">
       <header className="flex items-center justify-between gap-4 px-16">
-        <div className="racking-wide flex items-center justify-center gap-4 text-4xl font-semibold text-cyan-300">
-          <MdDateRange size={55} color="3A7DFF" /> RESERVER
+        <div className="racking-wide flex items-center justify-center gap-4 text-4xl font-semibold text-blue-800">
+          <MdDateRange size={55} /> RESERVER
         </div>
         <div className="flex gap-4">
           <SignUpButton />
@@ -40,15 +40,15 @@ function Index() {
         </div>
       </header>
       <main className="flex flex-col gap-12">
-        <div className="mx-20 flex flex-col items-center gap-4 border-b border-t border-gray-200 p-6 text-xl">
+        <div className="mx-20 flex flex-col items-center gap-4 border-b border-t border-gray-500 p-6 text-xl">
           <div>Reserva citas cómodas e inmediatas para sus clientes.</div>
-          <div className="text-2xl font-bold text-cyan-400">
+          <div className="text-2xl font-bold text-blue-800">
             El mejor software para agendar citas en Mexico
           </div>
         </div>
         <div className="self-center text-4xl font-bold">
           ¿Qué es{" "}
-          <strong className="self-center text-cyan-400">RESERVER</strong>?
+          <strong className="self-center text-blue-800">RESERVER</strong>?
         </div>
         <div className="flex justify-around gap-4">
           {info.map((el, index) => (
