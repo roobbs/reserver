48c331985ac8901e38b3389db0e5b70340b7789e
Carlos S. <carlossalmeroncabrera@gmail.com>
2024-09-10 11:43:47 -0600

    Added media querys to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,27 @@ export default {
         inter: ["Inter", "sans-serif"],
       },
     },
+    screens: {
+      "2xl": { max: "1535px" },
+      // => @media (max-width: 1535px) { ... }
+
+      xl: { max: "1279px" },
+      // => @media (max-width: 1279px) { ... }
+
+      lg: { max: "1023px" },
+      // => @media (max-width: 1023px) { ... }
+
+      md: { max: "767px" },
+      // => @media (max-width: 767px) { ... }
+
+      "1180p": { max: "1180px" },
+      "850p": { max: "850px" },
+      "755p": { max: "755px" },
+      "720p": { max: "720px" },
+      "650p": { max: "650px" },
+      "550p": { max: "550px" },
+      "500p": { max: "500px" },
+    },
   },
   plugins: [],
 };
