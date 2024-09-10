53b1c8407f8211cd82d393360cea81802e2ded92
Carlos S. <carlossalmeroncabrera@gmail.com>
2024-09-10 11:46:39 -0600

    Made Index.tsx responsive

--- src/screens/Index.tsx
+++ src/screens/Index.tsx
@@ -29,9 +29,9 @@ const info = [
 
 function Index() {
   return (
-    <div className="flex flex-1 flex-col gap-12 bg-white py-6 pl-8 text-blue-950">
-      <header className="flex items-center justify-between gap-4 px-16">
-        <div className="racking-wide flex items-center justify-center gap-4 text-4xl font-semibold text-blue-800">
+    <div className="flex flex-1 flex-col gap-12 bg-white py-6 text-blue-950">
+      <header className="720p:px-8 flex flex-wrap items-center justify-around gap-8 px-16">
+        <div className="flex items-center justify-center gap-4 text-4xl font-semibold text-blue-800">
           <MdDateRange size={55} /> RESERVER
         </div>
         <div className="flex gap-4">
@@ -40,9 +40,9 @@ function Index() {
         </div>
       </header>
       <main className="flex flex-col gap-12">
-        <div className="mx-20 flex flex-col items-center gap-4 border-b border-t border-gray-500 p-6 text-xl">
+        <div className="720p:mx-6 mx-20 flex flex-col items-center gap-4 border-b border-t border-gray-500 p-6 text-center text-xl">
           <div>Reserva citas cómodas e inmediatas para sus clientes.</div>
-          <div className="text-2xl font-bold text-blue-800">
+          <div className="text-center text-2xl font-bold text-blue-800">
             El mejor software para agendar citas en Mexico
           </div>
         </div>
@@ -50,7 +50,7 @@ function Index() {
           ¿Qué es{" "}
           <strong className="self-center text-blue-800">RESERVER</strong>?
         </div>
-        <div className="flex justify-around gap-4">
+        <div className="flex flex-wrap justify-center gap-4 p-2">
           {info.map((el, index) => (
             <InfoCard key={index} title={el.title} info={el.info} />
           ))}
